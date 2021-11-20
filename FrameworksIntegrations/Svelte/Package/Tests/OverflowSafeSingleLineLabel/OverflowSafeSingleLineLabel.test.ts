99771f233e253298faaf9e4fab0433418693dfa9
Takesi Tokugawa <tokugawa.takesi@gmail.com>
2021-11-20 11:59:38 +0900

    Interim

---
+++ FrameworksIntegrations/Svelte/Package/Tests/OverflowSafeSingleLineLabel/OverflowSafeSingleLineLabel.test.ts
@@ -0,0 +1,7 @@
+import OverflowSafeSingleLineLabelComponentTestSite from "./OverflowSafeSingleLineLabelComponentTestSite.svelte";
+
+
+new OverflowSafeSingleLineLabelComponentTestSite({
+  target: document.body
+})
+
