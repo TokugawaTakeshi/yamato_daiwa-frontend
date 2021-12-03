dc4eb892ba6a259c402ae754d532af917f672956
Takesi Tokugawa <tokugawa.takesi@gmail.com>
2021-12-03 21:37:06 +0900

    Interim

---
+++ FrameworksIntegrations/Vue/Package/Source/SVG_Icons/Bell/BellIcon__Filled.vue
@@ -0,0 +1,7 @@
+<template lang="pug">
+
+  include node_modules/@yamato-daiwa/frontend/Markup/Assets/SVG_Icons/Bell/BellIcon__Filled.pug
+
+  +Bell__Filled--Icon
+    
+</template>
--- FrameworksIntegrations/Vue/Package/Source/SVG_Icons/BellIcon__Filled.vue
+++
@@ -1,7 +0,0 @@
-<template lang="pug">
-
-  include ../../Markup/SVG_Icons/SVG_Icons
-
-  +Bell__Filled--Icon
-    
-</template>
